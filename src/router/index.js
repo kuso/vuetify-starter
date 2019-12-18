628c0bb7a16d0836266852e8b357a424ddc9304a
kuso <kuso>
2019-12-18 21:51:40 +0800

    init commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+  },
+  {
+    path: '/hello_world',
+    name: 'hello_world',
+    component: () => import('../views/HelloWorld'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello_world">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'hello_world',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
